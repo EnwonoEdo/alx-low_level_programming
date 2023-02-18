abd010cfe74ae9ecb02c63608edbf174da3aa9a3
EnwonoEdo <promiseedoinyang@gmail.com>
2023-02-17 21:00:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that print a line to the standard error
- * return 1 (success)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (success)
  */
 int main(void)
 {
