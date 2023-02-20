e74f74cceeed82e7ca499783dc4102b46a200b8f
EnwonoEdo <promiseedoinyang@gmail.com>
2023-02-20 12:12:34 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int i;
+	 int i;
 
-	for (i = '0'; <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 
 		if (i != '9')
 		{
 			putchar(',');
-			putchar('');
+			putchar(',');
 		}
 	}
 
