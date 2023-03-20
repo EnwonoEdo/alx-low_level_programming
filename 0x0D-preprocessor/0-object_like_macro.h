79ff20b75d564678fb7da14078928e61a4d0ca18
EnwonoEdo <promiseedoinyang@gmail.com>
2023-03-20 15:25:31 -0700

    C - Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Defines a macro named SIZE of value 1024
+ */
+
+#define SIZE 1024
+
+#endif
+
