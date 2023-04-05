508a9cb60a25306a7ca39e07f090430f546afcd3
EnwonoEdo <promiseedoinyang@gmail.com>
2023-04-04 17:36:54 -0700

    Update

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops node
+ * @head: list address
+ * Return: head node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	int value = 0;
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return (value);
+	tmp = *head;
+	value = tmp->n;
+	free(*head);
+	*head = tmp->next;
+
+	return (value);
+}
